d7f9621e017552af24c7e7f04616124ea1c03206
Stefano Sansone <stefanosansone.dev@gmail.com>
2024-12-04 12:59:05 +0100

    add 2015 day 01 part 2

--- src/2015/Day01.kt
+++ src/2015/Day01.kt
@@ -1,11 +1,20 @@
+package `2015`
+
+import println
+import readInput
+
 fun main() {
     fun part1(input: String) = input.fold(0) { acc, c -> acc + if (c == '(') 1 else if (c == ')') -1 else 0 }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: String): Int {
+        var floor = 0
+        return input.indexOfFirst {
+            floor += if (it == '(') 1 else -1
+            floor < 0
+        } + 1
     }
 
-    val input = readInput("Day01")
+    val input = readInput("2015","Day01")
     part1(input).println()
-    //part2(input).println()
+    part2(input).println()
 }
